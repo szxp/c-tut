a7fec0a417d3fd87ce07f8cef3baca991eb95297
Péter Szakszon <szp0530@gmail.com>
2025-10-30 19:20:51 +0100

    Printf

--- var/var.c
+++ var/var.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    int height = 8;
+    int length, width, height, volume;
+    length = 5;
+    width = 6;
+    height = 7;
+    volume = length * width * height;
+    printf("length: %d\n", length);
+    printf("width: %d\n", width);
+    printf("height: %d\n", height);
+    printf("volume: %d\n", volume);
+ 
     float f1 = 2150.48f;
-    printf("height = %d\n", height);
-    printf("f1 = %.3f\n", f1);
+    printf("f1: %.3f\n", f1);
     return 0;
 }
 
